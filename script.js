12227710706c9a291c6186a06eb666f1d06c79d1
manuvarshney22 <73613456+manuvarshney22@users.noreply.github.com>
2020-10-31 23:35:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 const apiKey = "12345678901234567890123456789012-12345678-12345678";
 const db = "abc123.database.secure.windows.net";
+
+function endsWith(x, y) {
+  return x.lastIndexOf(y) === x.length - y.length;
+}
